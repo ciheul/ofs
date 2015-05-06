5ec9698ce3e76f2b58d7670aa020a6ebd27ff63d
okiww <oxyjunkhead12@gmail.com>
2015-05-06 10:38:59 +0700

    add metro 3

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/jqueryui/jquery-ui.js',
       'bower_components/jquery-mousewheel/jquery.mousewheel.js',
-      'bower_components/metro-ui-css/min/metro.min.js',
+      'bower_components/metro-ui-css/build/js/metro.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
