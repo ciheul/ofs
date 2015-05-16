6dce71384c23e303b08861cfa4d87550d13c41b8
okiww <oxyjunkhead12@gmail.com>
2015-05-16 21:06:29 +0700

    solved eror route in srpDetail

--- app/scripts/controllers/srp.js
+++ app/scripts/controllers/srp.js
@@ -2,15 +2,16 @@
 
 var srpControllers = angular.module('srpControllers', []);
 
- srpControllers.controller('srpCtrl', ['$scope', '$http', '$routeParams', 
-  function ($scope, $routeParams, $http){
+srpControllers.controller('srpCtrl', ['$scope', '$http', '$routeParams', 
+  function($scope, $http, $routeParams) {
 
     console.log($routeParams.UnitId);
-    var param = {unitId: $route.Params.UnitId};
+    var param = {unitId: $routeParams.UnitId};
       $http.get('http://teleconscada-web00.cloudapp.net:1980/api/srpdetail/', {params: param})
-      .success(function(data){
-        $scope.dataId = data;
-    });
-      console.log($routeParams.UnitId);
+      	.success(function(data){
+        	$scope.dataId = data;
+    	});
   }]);
+
+
  
